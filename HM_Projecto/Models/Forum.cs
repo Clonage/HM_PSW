5758f08385ec699e413ffca79776212ecf23a449
Clonage <mitchel.mauricio@gmail.com>
2015-11-11 20:09:00 +0000

    Commit dos Modelos

---
+++ HM_Projecto/Models/Forum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.ComponentModel.DataAnnotations;
+
+namespace HM_Projecto.Models
+{
+    public class Forum
+    {
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private int id_Community;
+
+        public int ID_Community
+        {
+            get { return id_Community; }
+            set { id_Community = value; }
+        }
+
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+    }
+}
