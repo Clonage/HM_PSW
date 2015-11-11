db19c34090976bfd58c3a62cd1643c1e8072d3fe
Clonage <mitchel.mauricio@gmail.com>
2015-11-11 19:56:09 +0000

    Criação do projecto

---
+++ HM_Projecto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HM_Projecto.MvcApplication" Language="C#" %>
---
+++ HM_Projecto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HM_Projecto.Startup))]
+namespace HM_Projecto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
